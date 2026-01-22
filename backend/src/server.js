2165520e91bac34fedd0b855ef893b9581dff6f7
Alamos Technologies <157238856+LordAlamos@users.noreply.github.com>
2026-01-22 01:02:00 -0300

    alteraçãoteste1

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // ===== SERVIR FRONTEND =====
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'backend/frontend')));
 
 // ===== SERVIR UPLOADS =====
 app.use('/uploads', express.static(uploadDir, {
